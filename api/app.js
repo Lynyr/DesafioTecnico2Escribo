afa74726da8e552a798e3c73a047a6e41b418de2
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2023-11-30 12:33:32 -0300

    Add files via upload

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
-const sequelize = require('./database');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,10 +11,9 @@ app.use(bodyParser.json());
 app.use('/api', userRoutes);
 
 // Inicialização do servidor
-sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
   });
-});
+
 
 module.exports = app;
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,11 +1,18 @@
+// userRoutes.js
+
 const express = require('express');
 const { createUser, loginUser, getUserInfo } = require('../controllers/userController');
 const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
+// Rota para registro de usuário
 router.post('/register', createUser);
+
+// Rota para login
 router.post('/login', loginUser);
+
+// Rota protegida para obter informações do usuário
 router.get('/user', authMiddleware, getUserInfo);
 
 module.exports = router;
